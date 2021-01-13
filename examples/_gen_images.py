93d0b38f44dd4523847bc120fc0185579dd08506
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-01-12 23:40:04 -0500

    update example files

--- examples/_gen_images.py
+++ examples/_gen_images.py
@@ -3,4 +3,8 @@ garbo
 """
 from mpl_playback.record import record_file
 
-record_file("plot_file.py", "fig")
+# record_file("dynamic.py", "fig")
+# record_file("subplots.py", "ctrls.fig")
+# record_file("multifig.py", ["fig", "slider_fig"])
+record_file("multifig.py", ["slider_fig", "fig"])
+# record_file("multifig.py", ["slider_fig"])
---
+++ examples/_playback.py
@@ -0,0 +1,8 @@
+from mpl_playback.playback import playback_file
+
+# playback_file("_dynamic-playback.json", "dynamic.py", "ohboi.gif")
+# playback_file("_subplots-playback.json", "subplots.py", "ohboi.gif")
+playback_file("_multifig-playback.json", "multifig.py", ["multi1.gif", "multi2.gif"])
+# playback_file("_multifig-playback.json", "multifig.py", ["multi1-garbo.gif", "multi2-garbo.gif"])
+
+# playback_file("_multifig-playback.json", "multifig.py", ["multi.gif"])
